1ea2abe5ea4192d9b88ec66ea24975464d0e7a6a
Marcus Gregersen <mgmano@gmail.com>
2015-07-13 16:31:47 +0200

    Routes and partials

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,31 @@
-angular.module('newsletter', ['ngRoute']);
+var newsletterController = angular.module('newsletterController', []);
+
+newsletterController.controller('newsletterController', ['$scope', '$routeParams',
+  function($scope, $routeParams) {
+    //TODO
+  }]);
+
+var newsletterApp = angular.module('newsletter', [
+  'ngRoute',
+  'newsletterController'
+]).controller('faqController', ['$scope', '$routeParams',
+function($scope, $routeParams) {
+  //TODO
+}]);
+
+
+newsletterApp.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/', {
+        templateUrl: 'assets/partials/newletters.html',
+        controller: 'newsletterController'
+      }).
+      when('/faq', {
+        templateUrl: 'assets/partials/faq.html',
+        controller: 'faqController'
+      }).
+      otherwise({
+        redirectTo: '/'
+      });
+  }]);
