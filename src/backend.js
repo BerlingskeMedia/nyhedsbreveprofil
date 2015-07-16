6e44727cc0f9b49706ce80bed5676b0944cce722
Marcus Gregersen <mgmano@gmail.com>
2015-07-16 12:38:50 +0200

    Generic proxy

--- src/backend.js
+++ src/backend.js
@@ -4,62 +4,13 @@ var backend = {
   register: function (plugin, options, next) {
 
     plugin.route({
-      method: 'get',
-      path: '/publishers',
-      handler: {
-        proxy: {
-          uri: 'http://54.77.4.249:8000/publishers'
-        }
-    }
-    });
-
-    plugin.route({
-      method: 'get',
-      path: '/nyhedsbreve',
-      handler: {
-        proxy: {
-          uri: 'http://54.77.4.249:8000/nyhedsbreve'
-        }
-    }
-    });
-
-    plugin.route({
-      method: 'get',
-      path: '/interesser',
-      handler: {
-        proxy: {
-          uri: 'http://54.77.4.249:8000/interesser?displayTypeId=3'
-        }
-    }
-    });
-
-    plugin.route({
-      method: 'post',
-      path: '/users',
-      handler: {
-        proxy: {
-          uri: 'http://54.77.4.249:8000/users'
-        }
-    }
-    });
-
-    plugin.route({
-      method: 'post',
-      path: '/mails/profile-page-link',
-      handler: {
-        proxy: {
-          uri: 'http://54.77.4.249:8000/mails/profile-page-link'
-        }
-    }
-    });
-
-
-    plugin.route({
       method: ['get', 'put', 'post', 'patch'],
-      path: '/users/{ekstern_id}/interesser',
+      path: '/{path*}',
       handler: {
         proxy: {
-          uri: 'http://54.77.4.249:8000/users/11ffbd194645c9b3fbda06bf62dbb17b/interesser'
+          mapUri: function(request, callback) {
+            callback(null, 'http://54.77.4.249:8000/' + request.params.path);
+          } //
         }
     }
     });
