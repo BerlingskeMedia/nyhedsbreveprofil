cb7dc32c4f7a12610a755efadeadb80b6252fd96
Daniel Kokott <dako@berlingskemedia.dk>
2016-05-03 11:10:16 +0200

    html base href /

--- src/opdatering.js
+++ src/opdatering.js
@@ -3,30 +3,12 @@
 
 module.exports.register = function (server, options, next) {
 
-  // server.route({
-  //   method: 'get',
-  //   path: '/build/{param*}',
-  //   handler: {
-  //     directory: {
-  //       path: 'opdatering/build'
-  //     }
-  //   }
-  // });
-  //
-  // server.route({
-  //   method: 'get',
-  //   path: '/{param*}',
-  //   handler: {
-  //     file: 'opdatering/index.html'
-  //   }
-  // });
-
   server.route({
     method: 'get',
-    path: '/bower_components/{param*}',
+    path: '/build/{param*}',
     handler: {
       directory: {
-        path: 'bower_components'
+        path: 'opdatering/build'
       }
     }
   });
@@ -35,19 +17,27 @@ module.exports.register = function (server, options, next) {
     method: 'get',
     path: '/{param*}',
     handler: {
-      directory: {
-        path: 'opdatering'
-      }
-    }
-  });
-
-  server.route({
-    method: 'get',
-    path: '/',
-    handler: {
       file: 'opdatering/index.html'
     }
   });
+
+  // server.route({
+  //   method: 'get',
+  //   path: '/{param*}',
+  //   handler: {
+  //     directory: {
+  //       path: 'opdatering'
+  //     }
+  //   }
+  // });
+  //
+  // server.route({
+  //   method: 'get',
+  //   path: '/',
+  //   handler: {
+  //     file: 'opdatering/index.html'
+  //   }
+  // });
 
   next();
 };
