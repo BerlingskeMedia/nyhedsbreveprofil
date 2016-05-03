55ff6060b3c1e7cd97b5d0921bac115c52ec0234
Daniel Kokott <dako@berlingskemedia.dk>
2016-05-03 13:42:10 +0200

    trying to eliminate the browser.min.js

--- opdatering/app/main.js
+++ opdatering/app/main.js
@@ -1,6 +1,6 @@
+var $ = require("jquery");
 var React = require('react');
 var ReactDOM = require('react-dom');
-var $ = require("jquery");
 var StepStamdata = require('./step_stamdata');
 var StepInteresser = require('./step_interesser');
 var StepNyhedsbreveKom = require('./step_nyhedsbreve_redaktionelle');
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,17 @@
 var path = require('path');
 
 module.exports = {
+    target: 'web',
     entry: path.join(__dirname, 'opdatering/app/main.js'),
+    // entry: ['babel-core', path.join(__dirname, 'opdatering/app/main.js')],
     output: {
         path: path.join(__dirname, 'opdatering/build'),
         filename: "bundle.js"
     },
     module: {
         loaders: [
-            { test: /\.css$/, loader: "style!css" },
-            { test: /\.json$/, loader: 'json-loader'},
+            { test: /\.css$/, loader: 'style!css' },
+            { test: /\.json$/, loader: 'json-loader' },
             { test: path.join(__dirname, 'opdatering/app'), loader: 'babel-loader' }
         ]
     }
