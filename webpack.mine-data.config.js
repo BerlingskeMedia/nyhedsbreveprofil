318ec252deba1eabc3cf0afecb9ed33354096f9a
Daniel Jozie Kokott <dako@berlingskemedia.dk>
2018-03-15 12:41:45 +0100

    renamed app to src

--- webpack.mine-data.config.js
+++ webpack.mine-data.config.js
@@ -2,8 +2,8 @@ var path = require('path');
 
 module.exports = {
     target: 'web',
-    entry: path.join(__dirname, 'mine-data/app/main.js'),
-    // entry: ['babel-core', path.join(__dirname, 'opdatering/app/main.js')],
+    entry: path.join(__dirname, 'mine-data/src/main.js'),
+    // entry: ['babel-core', path.join(__dirname, 'opdatering/src/main.js')],
     output: {
         path: path.join(__dirname, 'mine-data/build'),
         filename: "bundle.js"
@@ -12,7 +12,7 @@ module.exports = {
         loaders: [
             { test: /\.css$/, loader: 'style!css' },
             { test: /\.json$/, loader: 'json-loader' },
-            { test: path.join(__dirname, 'mine-data/app'), loader: 'babel-loader', query: { presets: ['react', 'es2015'] } }
+            { test: path.join(__dirname, 'mine-data/src'), loader: 'babel-loader', query: { presets: ['react', 'es2015'] } }
         ]
     }
 };
