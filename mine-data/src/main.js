7c61ab3c0ca3e8facbad88588c273a9407a9cf2c
Daniel Jozie Kokott <dako@berlingskemedia.dk>
2018-03-27 11:36:06 +0200

    Some categories

--- mine-data/src/main.js
+++ mine-data/src/main.js
@@ -13,7 +13,17 @@ class MineData extends React.Component {
   render() {
 
     return (
-      <h1>Mine data</h1>
+      <div>
+        <h1>Mine data</h1>
+        <h2>Abonnementsregister</h2>
+        <h2>BT-Shop/Berlingske Shop</h2>
+        <h2>Internt produktionsregister til rapportering mv.</h2>
+        <h2>Kundeunivers.dk</h2>
+        <h2>Mail system</h2>
+        <h2>Marketingsregister til markedsundersøgelser</h2>
+        <h2>Marketingsregister til salg og marketing</h2>
+        <h2>Marketingsregister til telemarketing</h2>
+      </div>
     );
   }
 }
