5677c788f43921c66cdb2ef555e31b41066d74d0
xmaga <xmaga@berlingskemedia.dk>
2018-04-24 09:45:02 +0200

    Added some user information in UserDetails.

--- mine-data/src/UserDetails/UserDetails.js
+++ mine-data/src/UserDetails/UserDetails.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Card, CardBody } from 'reactstrap';
+import { Card, CardBody, CardSubtitle, CardTitle } from 'reactstrap';
 
 import './UserDetails.scss';
 
-const DetailsCard = ({userInfo}) => (
+const DetailsCard = ({user}) => (
   <Card className="UserDetails">
     <CardBody>
-      foo bar
+      <CardTitle>{user.firstName} {user.lastName}</CardTitle>
+      <CardSubtitle>{user.email}</CardSubtitle>
+      <div className="UserDetails-info">
+        <div className="UserDetails-info-row">
+          {user.nickname ? <div>nick name: {user.nickname}</div> : null}
+          {user.age ? <div>age: {user.age}</div> : null}
+          {(user.birthDay && user.birthMonth && user.birthYear) ?
+            <div>date of birth: {`${user.birthYear}-${user.birthMonth}-${user.birthDay}`}</div> : null}
+        </div>
+        <div className="UserDetails-info-row">
+          {user.address ? <div>address: {user.address}</div> : null}
+          {user.city ? <div>city: {user.city}</div> : null}
+          {user.country ? <div>country: {user.country}</div> : null}
+        </div>
+        {user.bio ? <div>bio: {user.bio}</div> : null}
+      </div>
     </CardBody>
     <div className="UserDetails-avatar">
 
@@ -16,7 +31,7 @@ const DetailsCard = ({userInfo}) => (
 );
 
 const mapStateToProps = ({userInfo}) => ({
-  userInfo
+  user: userInfo.userInfo.profile
 });
 
 export const UserDetails = connect(mapStateToProps)(DetailsCard);
