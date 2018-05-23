340603b78d71e9ef87f2fd46371289ad4cdb050f
bmxpiku <xpiku@berlingskemedia.dk>
2018-05-23 13:33:30 +0200

    Add email pending message modal review changes

--- mine-data/src/VerifyEmail/VerifyPending.js
+++ mine-data/src/VerifyEmail/VerifyPending.js
@@ -1,12 +1,11 @@
 import React, {Fragment} from 'react';
 import SubmitButton from '../SubmitButton/SubmitButton';
-import {Modal, ModalBody, ModalFooter} from "reactstrap";
 
 export class VerifyPending extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {sent: false};
+
     this.submit = this.submit.bind(this);
     this.redirect = this.redirect.bind(this);
   }
@@ -26,8 +25,6 @@ export class VerifyPending extends React.Component {
       regToken: this.props.location.state.regToken,
       email: this.props.location.state.username
     });
-
-    this.setState({sent: true});
   }
 
   render() {
@@ -37,15 +34,9 @@ export class VerifyPending extends React.Component {
         <p>
           For din sikkerhed er der sendt en verifikations-email til dig.<br/>
           Følg instruktionerne i e-mailen for at bekræfte din konto.
-          <br/>For at sende bekræftelses-e-mailen igen, skal du klikke på Send.
-          <SubmitButton onClick={this.submit}>Send</SubmitButton>
+          <br/>
+          <SubmitButton onClick={this.submit}>Home</SubmitButton>
         </p>
-        <Modal isOpen={this.state.sent} toggle={this.redirect}>
-          <ModalBody>En bekræftelses email med et link til at bekræfte din konto er blevet sendt til dig.</ModalBody>
-          <ModalFooter>
-            <SubmitButton onClick={this.redirect}>OK</SubmitButton>
-          </ModalFooter>
-        </Modal>
       </Fragment>
     );
   }
