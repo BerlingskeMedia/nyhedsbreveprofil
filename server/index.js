465c49319ed13c4d124148871a9bf0d8059fc4f0
bmdako <dako@berlingskemedia.dk>
2020-03-17 22:53:21 +0100

    old code

--- server/index.js
+++ server/index.js
@@ -15,8 +15,6 @@ const nyhedsbreve = require('./nyhedsbreve');
 const smartlinks = require('./smartlinks');
 const opdatering = require('./opdatering');
 const mineData = require('./mine-data');
-// const hapiAuthJwt2 = require('hapi-auth-jwt2');
-// const { authConfig } = require('./lib/jwt');
 
 const init = async () => {
 
@@ -25,17 +23,6 @@ const init = async () => {
       host: 'localhost'
   });
 
-
-  // await server.register(hapiAuthJwt2);
-  // server.auth.strategy('jwt', 'jwt',
-  // { key: process.env.JWT_SECRET, // Never Share your secret key
-    // validate: authConfig.validateFunc  // validate function defined above
-  // });
-
-  // server.auth.default('jwt');
-
-  // server.auth.strategy('jwt', 'jwt', authConfig);
-
   server.route({
     method: 'GET',
     path: '/healthcheck',
@@ -43,7 +30,6 @@ const init = async () => {
       return 'OK';
     }
   });
-
 
   await server.register(inert);
   
@@ -61,47 +47,3 @@ const init = async () => {
 };
 
 init();
-
-// var server = new Hapi.Server({
-//   connections: {
-//     router: {
-//       stripTrailingSlash: false
-//     }
-//   }
-// });
-
-
-
-// server.connection({
-//   port: process.env.PORT ? process.env.PORT : 8000,
-//   routes: {
-//     cors: true
-//   }
-// });
-
-// server.register(hapiAuthJwt2, err => {
-//   if (err) {
-//     console.log(err);
-//   }
-
-
-
-//   if (!module.parent) {
-//     server.start((err) => {
-//       if (err) {
-//         throw err;
-//       }
-
-//       console.log(`Server running at: ${server.info.uri}`);
-//     });
-//   }
-// });
-
-// function cb (err) {
-//   if (err) {
-//     console.log('Error when loading plugin', err);
-//     server.stop();
-//   }
-// }
-
-// module.exports = server;
