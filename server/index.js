0439ff468be6a3e7c91bc20d3e91bb40a434e8a4
Mateusz Zachciał <mzachcial@pl.sii.eu>
2020-11-26 12:59:58 +0100

    BMD-4599 WIP

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 /*jshint node: true */
 'use strict';
 
-const.log('MA ', process.env.MDBAPI_ADDRESS);
+console.log('MA ', process.env.MDBAPI_ADDRESS);
 
 // To remain compatible with the puppet-scripts
 if(process.env.NYHEDSBREVEPROFIL_APP_ID && !process.env.BPC_APP_ID) {
