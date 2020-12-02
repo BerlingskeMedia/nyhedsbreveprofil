0db9f2516f18e6826a98c5c3361d43a4d09f255d
Mateusz Zachciał <mzachcial@pl.sii.eu>
2020-12-02 14:41:52 +0100

    BMD-4599 WIP

--- server/api.js
+++ server/api.js
@@ -18,6 +18,7 @@ try {
   // Sometimes the ENV var is including the protocol, eg: MDBAPI_ADDRESS=http://mdbapi-test.bemit.dk
 
   if(['http:', 'https:'].indexOf(temp.protocol) > -1) {
+    console.log('Option 1');
     
     MDBAPI_PROTOCOL = temp.protocol;
     MDBAPI_HOSTNAME = temp.hostname;
@@ -26,6 +27,7 @@ try {
   // Other times (eg. in puppet) there are two seperate ENV vars, eg: MDBAPI_ADDRESS=mdbapi-test.bemit.dk MDBAPI_PORT=80
 
   } else if (process.env.MDBAPI_PORT) {
+    console.log('Option 2');
 
     MDBAPI_PROTOCOL = 'http:';
     MDBAPI_HOSTNAME = process.env.MDBAPI_ADDRESS;
